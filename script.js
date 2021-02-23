38701ea02d56374d2d551f2b85d82b605518a2d3
ArvinLeung <arvin.leung@ocsbstudent.ca>
2021-02-23 15:48:43 +0000

    space added to css

--- script.js
+++ script.js
@@ -1,13 +1,19 @@
+// This allows the button to call the functio when clicked
 document.getElementById('click').addEventListener('click', guesser)
 
+/* The function below checks if the guess is greater or lower than the actual age using a loop */
 function guesser () {
+// This stores 15 into the variable myage
 let myage = 15
+// This starts a while loop where the condition is if myage doesnt equal answer
 while (myage !== answer) {
+  /* The line below loops the prompt and sets the answer to the prompt as the variable answer */
  var answer = prompt('Please guess my age?') 
+// The lines below check if the guess is correct or not
   if (answer < myage) {
-    alert('Too low!') 
+    alert('Too low! Please try again') 
   } else if (answer > myage) {
-    alert('Too high! Please guess what is my age?') 
+    alert('Too high! Please try again') 
   } else if (answer = myage) {
     alert('Correct!')
   } else {
@@ -15,8 +21,3 @@ while (myage !== answer) {
   }
 }
 }
-
-// document.getElementById('result').innerHTML = "Too high! Please guess what is my age?"
-// document.getElementById('result').innerHTML = "Too low! Please guess what is my age?"
-// alert('Too high! Please guess what is my age?') 
-// alert('Too low! Please guess what is my age?') 
